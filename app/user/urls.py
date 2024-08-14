982b16d204b8e3c0ecff1f127033c75550c82d62
csuonereal <csuonereal@gmail.com>
2024-08-14 15:15:44 +0300

    Implement user profile management with retrieve and update functionality
    - Add `ManageUserView` to handle authenticated user profile retrieval and updates.
    - Update URL patterns to include `me/` endpoint for user profile management.
    - Enhance test cases to verify user profile retrieval and update operations for authenticated users.
    - Ensure password is hashed correctly during profile updates.

--- app/user/urls.py
+++ app/user/urls.py
@@ -3,11 +3,12 @@ Urls mapping for user API
 """
 
 from django.urls import path
-from .views import CreateUserView, CreateTokenView
+from .views import CreateUserView, CreateTokenView, ManageUserView
 
 app_name = "user"  # for reverse mapping
 
 urlpatterns = [
     path("create/", CreateUserView.as_view(), name="create"),
     path("token/", CreateTokenView.as_view(), name="token"),
+    path("me/", ManageUserView.as_view(), name="me"),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,6 @@
 """Views for the user API"""
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from .serializers import UserSerializer, AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
@@ -20,3 +20,22 @@ class CreateTokenView(ObtainAuthToken):
     renderer_classes = (
         api_settings.DEFAULT_RENDERER_CLASSES
     )  # to make the view browsable
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+
+    serializer_class = UserSerializer
+    # meaning the view expects a token to authenticate users.
+    # authentication is the process of verifying who a user is
+    authentication_classes = (authentication.TokenAuthentication,)
+    # ensures that only authenticated users can access this view
+    # permission checks what an authenticated user is allowed to do
+    permission_classes = (permissions.IsAuthenticated,)
+
+    # this method is overridden to change the default behavior of retrieving an object by ID or other filters. Instead, it directly returns the authenticated user
+    def get_object(self):
+        """Retrieve and return authenticated user"""
+        return (
+            self.request.user
+        )  # eturns the authenticated user  which is the user associated with the current session or token.
