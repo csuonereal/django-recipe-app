116b3e7171303a3f648874f1f1a27befc38ecefe
csuonereal <csuonereal@gmail.com>
2024-08-17 16:51:50 +0300

    Add image field and upload path for Recipe model
    Create migration to add `image` field to the `Recipe` model. Define a function to generate unique file paths for uploaded images using UUIDs. Update tests to verify correct file path generation for recipe images.

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,8 @@
 Database models
 """
 
+import uuid
+import os
 from django.db import models
 
 # AbstractBaseUser is a class that implements the core of the user model
@@ -12,6 +14,15 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.conf import settings
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image"""
+    ext = filename.split(".")[-1]  # -1 means the last element of the list
+    filename = f"{uuid.uuid4()}.{ext}"
+
+    return os.path.join("uploads/recipe/", filename)  # its base url is MEDIA_ROOT
+    # return os.path.join("upload", "recipe", filename) #its base url is MEDIA_ROOT
 
 
 class UserManager(BaseUserManager):
@@ -57,11 +68,9 @@ class Recipe(models.Model):
     """Recipe object"""
 
     # one user can have multiple recipes but one recipe can only have one user
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,  # AUTH_USER_MODEL is the user model that is active in the project
-        on_delete=models.CASCADE,
-        # CASCADE means if the user is deleted, all the recipes associated with that user will also be deleted
-    )
+    # AUTH_USER_MODEL is the user model that is active in the project
+    # CASCADE means if the user is deleted, all the recipes associated with that user will also be deleted
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField()
     time_minutes = models.IntegerField()
@@ -69,6 +78,8 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField("Tag")
     ingredients = models.ManyToManyField("Ingredient")
+    # we are passing the function reference, not calling the function
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
